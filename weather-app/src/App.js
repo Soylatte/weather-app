d0bf742141fac8683159c3eab450b0988bbf4a56
Soyeon Kim <kksoyeon61@gmail.com>
2023-12-07 15:51:53 +0900

    add input

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [data,setData] = useState({});
+  const [location,setLocation] = useState('');
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=Seoul,kr&appid=18e223077337135dae9e1d2c9296858b'
+ const url = 'https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=18e223077337135dae9e1d2c9296858b'
+
+ const searchLocation = (event) => {
+
+  if (event.key === 'Enter'){
+    axios.get(url).then((response)=>{
+      setData(response.data)
+      console.log(response.data)
+    })
+
+  }
+ }
+
+
   return (
-  <div className="App">
+  <div className="app">
+    <div className="search">
+      <input value={location}
+      onChange={event => setLocation(event.target.value)}
+      placeholder='Enter Location'
+      onKeyDown={searchLocation}
+      type='text'>
+      </input>
+    </div>
     <div className="container">
       <div className="top">
         <div className="location">
@@ -19,13 +42,16 @@ function App() {
               </div>
               <div className="bottom">
                 <div className="feels">
-                  <p>8℃</p>
+                  <p className='bold'>8℃</p>
+                  <p>Feels Like</p>
                 </div>
                 <div className="humidity">
                   <p>57%</p>
+                  <p>Humidity</p>
                   </div>
                   <div className="wind">
-                    12 MPH
+                    <p className='bold'>12 MPH</p>
+                    <p>Wind Speed</p>
                   </div>
 
               </div>
